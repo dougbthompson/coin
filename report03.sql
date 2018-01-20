a1a6e97b0dabdf3e92e268c0014a6c9a31c5679a
dougbthompson <doug.askaar@gmail.com>
2018-01-19 17:06:30 -0800

    20180119 17:06

--- report03.sql
+++ report03.sql
@@ -6,7 +6,7 @@
 
 drop procedure if exists report03;
 delimiter //
-create procedure report03(in zhours int)
+create procedure report03()
 begin
 
     -- "keys" are the list of symbols being tracked
@@ -27,19 +27,39 @@ begin
 
     -- scroll through the list of 1418 symbols
     set @idx = 0;
-    set @xlength = 10;
+    -- set @xlength = 10;
+
+    truncate table js3;
+    truncate table cmc_data;
 
     label1: loop
 
         if @idx = @xlength then
             leave label1;
         else -- the list of symbols
-            set @sql = concat("select json_extract(x, '$[",@idx,"]') from js;");
+            set @sql = concat("select json_extract(x, '$[",@idx,"]') into @symbol from js;");
             prepare stmt1 from @sql;
             execute stmt1;
             deallocate prepare stmt1;
 
-            select '2', @idx;
+            select @idx, @symbol;
+
+            truncate table js3;
+            set @sql = concat("insert into js3(x) select distinct json_unquote(x->'$.",@symbol,"') from cmc;");
+            prepare stmt1 from @sql;
+            execute stmt1;
+            deallocate prepare stmt1;
+
+            update js3 set x = json_set(x, '$.volume_usd_24h',     '0.0') where json_unquote(x->'$.volume_usd_24h')     = 'null';
+            update js3 set x = json_set(x, '$.percent_change_1h',  '0.0') where json_unquote(x->'$.percent_change_1h')  = 'null';
+            update js3 set x = json_set(x, '$.percent_change_24h', '0.0') where json_unquote(x->'$.percent_change_24h') = 'null';
+            update js3 set x = json_set(x, '$.percent_change_7d',  '0.0') where json_unquote(x->'$.percent_change_7d')  = 'null';
+
+            set @sql = concat("insert into cmc_data (cmc_coin_id, last_updated, last_actual_dt, last_actual_ts, volume_usd_24h, pc_1h, pc_24h, pc_7d, price_usd, price_btc, rank) select (select cmc_coin_id from cmc_coin where cmc_symbol = '",@symbol,"'), json_unquote(x->'$.last_updated'), json_unquote(x->'$.date_actual'), round(unix_timestamp(json_unquote(x->'$.date_actual')),0), json_unquote(x->'$.volume_usd_24h'), json_unquote(x->'$.percent_change_1h'), json_unquote(x->'$.percent_change_24h'), json_unquote(x->'$.percent_change_7d'), json_unquote(x->'$.price_usd'), json_unquote(x->'$.price_btc'), json_unquote(x->'$.rank') from js3;");
+            prepare stmt1 from @sql;
+            execute stmt1;
+            deallocate prepare stmt1;
+
             set @idx = @idx + 1;
             iterate label1;
         end if;
@@ -50,38 +70,5 @@ end
 //
 delimiter ;
 
-
-truncate table js; 
-insert into js(x) select json_unquote(x->'$.BTC') from cmc limit 1;
-
--- = 16
-select json_length(json_keys(x->'$.BTC')) from cmc limit 1;
-select json_extract(json_keys(x->'$.BTC'), '$[*]') from cmc limit 1;
-
-select * from cmc_coin where cmc_symbol = 'BTC';
-select cmc_coin_id from cmc_coin where cmc_symbol = 'BTC';
-
-truncate table js3;
-
-insert into js3(x) select distinct json_unquote(x->'$.BTC') from cmc;
-
-insert into cmc_data (cmc_coin_id, last_updated, last_actual_dt, last_actual_ts,
-       volume_usd_24h, pc_1h, pc_24h, pc_7d, price_usd, price_btc, rank)
-select (select cmc_coin_id from cmc_coin where cmc_symbol = 'BTC'),
-       json_unquote(x->'$.last_updated'),
-       json_unquote(x->'$.date_actual'),
-       round(unix_timestamp(json_unquote(x->'$.date_actual')),0),
-       json_unquote(x->'$.volume_usd_24h'),
-       json_unquote(x->'$.percent_change_1h'),
-       json_unquote(x->'$.percent_change_24h'),
-       json_unquote(x->'$.percent_change_7d'),
-       json_unquote(x->'$.price_usd'),
-       json_unquote(x->'$.price_btc'),
-       json_unquote(x->'$.rank')
-  from js3;
-
-insert into cmc_data (cmc_data_id, last_updated, last_actual_dt, last_actual_ts,
-       volume_usd_24h, pc_1h, pc_24h, pc_7d, price_usd, price_btc, rank)
-select 
-
+-- update js3 set x = json_set(x, '$.percent_change_24h', '0.0') where json_unquote(x->'$.percent_change_24h') = 'null';
 
