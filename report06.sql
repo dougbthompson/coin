2b456254207525daf1c14e97c7341e6482a57d30
dougbthompson <doug.askaar@gmail.com>
2018-01-25 08:24:26 -0800

    20180125 08:24

--- report06.sql
+++ report06.sql
@@ -4,14 +4,24 @@
 --
 
 create table cmc_baseline (
-    cmc_coin_id  integer, -- bitcoin, ethereum, dbt100
+    cmc_coin_id  integer,  -- bitcoin, ethereum, dbt100
     cmc_run_dt   datetime,
     cmc_run_lst  integer,
-    
+    delta        json,     -- {"H01":"-1.27","H04":"-2.46","H08":"1.01","H16":"4.35","H32":"8.27"}
+    index ix01_cmc_baseline (cmc_coin_id, cmc_run_lst)
 ) engine = innodb;
 
 create table cmc_beta (
     cmc_coin_id   integer,
     cmc_
 ) engine = innodb;
+
+
+select round(sum(volume_usd_24h/1000000.0),2) as TradeB,
+       round(avg(pc_1h),2)  as PC01H,
+       round(avg(pc_24h),2) as PC24H,
+       round(avg(pc_7d),2)  as PC07D
+  from cmc_data
+ where last_actual_dt  = '2018-01-16 14:15:00'
+   and rank           <= 100;
 
