cfcd75d4f610a468a425b1bec6b26a2993e3f40a
dougbthompson <doug.askaar@gmail.com>
2018-01-30 07:54:43 -0800

    20180130 07:54

--- report06_func.sql
+++ report06_func.sql
@@ -16,13 +16,4 @@ end
 delimiter ;
 
 
-    select a.last_actual_ts,
-           json_object('VOLUME', round(sum(a.volume_usd_24h/1000000.0),2),
-                       'PC01H', round(avg(pc_1h),2)
-           ) as json_value
-      from cmc_data a, cmc_time b
-     where a.last_actual_ts  = b.actual_lst
-       and a.rank           <= 100
-     group by a.last_actual_ts
-     limit 8;
 
