339c07c6261c30fcf200956bb20f18bb6d8ac39d
dougbthompson <doug.askaar@gmail.com>
2018-01-30 07:55:00 -0800

    20180130 07:55

--- report06_func.sql
+++ report06_func.sql
@@ -9,6 +9,16 @@ begin
     select 'return value' into @rvalue;
     cmc_correlation_hour.nhour
 
+    select a.last_actual_ts,
+           json_object('VOLUME', round(sum(a.volume_usd_24h/1000000.0),2),
+                       'PC01H', round(avg(pc_1h),2)
+           ) as json_value
+      from cmc_data a, cmc_time b
+     where a.last_actual_ts  = b.actual_lst
+       and a.rank           <= 100
+     group by a.last_actual_ts
+     limit 8;
+
     return @rvalue;
 end 
 //
