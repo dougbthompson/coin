7481eaf00c41f0f3fd8e04868c8fa410cb6a331b
dougbthompson <doug.askaar@gmail.com>
2018-06-05 07:23:02 -0700

    20180605 07:23

---
+++ zupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+typeset -i VAL=`echo $((0 + RANDOM % 8))`
+
+while [ ${VAL} -gt 0 ]
+do
+    echo "Val: ${VAL}) "
+    echo "--" >> /opt/coins/coinmarketcap/report02.sql; gitt.sh
+
+    (( VAL = VAL - 1 ))
+done
+
