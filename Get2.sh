c588e5a2e18b381e355a9a90832ecd5efadf6eac
dougbthompson <doug.askaar@gmail.com>
2019-01-22 09:13:07 -0800

    20190122 09:13

--- Get2.sh
+++ Get2.sh
@@ -27,8 +27,8 @@ export zDT="${zYE}-${zMO}-${zDA} ${zHO}:${zMI}"
 
 cat ${F1} | tr '\n' ' ' | sed -f sed.sed | sed -e "s/}}/},\"DATE\":\"${zDT}\"}/" > ${BCP}
 
-mysql coins -e "LOAD DATA local INFILE '${BCP}' replace INTO TABLE mph (x);"
-mysql coins -e "update mph set lstd = unix_timestamp(lst) where lstd is null;"
+# mysql coins -e "LOAD DATA local INFILE '${BCP}' replace INTO TABLE mph (x);"
+# mysql coins -e "update mph set lstd = unix_timestamp(lst) where lstd is null;"
 
 exit
 
