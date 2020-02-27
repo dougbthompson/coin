53dcbed98c9f0d74d236577d913e2b3cb09ebf86
dougbthompson <doug.askaar@gmail.com>
2020-02-27 09:10:25 -0800

    20200227 09:10

--- GetRep04.sh
+++ GetRep04.sh
@@ -4,8 +4,9 @@ cd /opt/coins/coinmarketcap
 
 export DT=`date "+%Y-%m-%d-%H-%M"`
 export YR=`date "+%Y"`
+export MO=`date "+%m"`
 
-export FILE="${YR}rep04/rpt.${DT}.txt"
+export FILE="${YR}rep04/${MO}/rpt.${DT}.txt"
 
 cat rep04.sql | mysql --table coins > ${FILE}
 
--- GetRep05.sh
+++ GetRep05.sh
@@ -4,8 +4,9 @@ cd /opt/coins/coinmarketcap
 
 export DT=`date "+%Y-%m-%d-%H-%M"`
 export YR=`date "+%Y"`
+export MO=`date "+%m"`
 
-export FILE="${YR}rep05/rpt.${DT}.txt"
+export FILE="${YR}rep05/${MO}/rpt.${DT}.txt"
 
 cat rep05.sql | mysql --table coins > ${FILE}
 
