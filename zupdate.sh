11431982d33a8452428e7874f8763bee5c19f5e7
dougbthompson <doug.askaar@gmail.com>
2021-08-01 08:31:56 -0700

    20210801 08:31

--- zupdate.sh
+++ zupdate.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-typeset -i VAL=$1
-
 cd /opt/coin
 
-# echo "Val- ${VAL}) "
-if [ "${VAL}" -eq 0 ]; then
-    typeset -i VAL=`echo $((0 + RANDOM % 8))`
+typeset -i RAN=`echo $((1 + $RANDOM % 10))`
+if [ ${RAN} -gt 6 ]; then 
+    exit
 fi
-# echo "Val- ${VAL}) "
-# echo ""
+
+typeset -i VAL=`echo $((1 + $RANDOM % 8))`
 
 while [ ${VAL} -gt 0 ]
 do
     echo "Val: ${VAL}) "
-    echo "--" >> /opt/coins/coinmarketcap/report02.sql; gitt.sh
+    echo "--" >> /opt/coins/coinmarketcap/report02.sql
+    /home/adminuser/gitt.sh
 
     (( VAL = VAL - 1 ))
 done
